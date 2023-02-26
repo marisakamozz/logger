8082cd378395c9730d7b388f16e058f0976a255b
Marisaka Mozz <38371671+marisakamozz@users.noreply.github.com>
2023-02-26 10:40:35 +0900

    add a new feature

--- log.go
+++ log.go
@@ -6,6 +6,8 @@ import (
 
 var Version string = "1.0"
 
-func Log(message string) {
-	fmt.Println("[LOG] " + message)
+func Log(message string, count int) {
+	for i := 0 ; i < count; i++ {
+		fmt.Println("[LOG] " + message)
+	}
 }
